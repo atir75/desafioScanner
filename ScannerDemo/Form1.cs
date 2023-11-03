f22a0cfad7235fa8ffc3a1e4604109cc68bfde25
Maria Rita <101608517+atir75@users.noreply.github.com>
2023-11-03 13:59:27 -0300

    first commit

---
+++ ScannerDemo/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIA;
+
+namespace ScannerDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListScanners();
+
+            textBox1.Text = Path.GetTempPath();
+            comboBox1.SelectedIndex = 1;
+
+        }
+
+        private void ListScanners()
+        {
+            listBox1.Items.Clear();
+
+            var deviceManager = new DeviceManager();
+            for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+            {
+                if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                {
+                    continue;
+                }
+
+                listBox1.Items.Add(
+                    new Scanner(deviceManager.DeviceInfos[i])
+                );
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(StartScanning).ContinueWith(result => TriggerScan());
+        }
+
+        private void TriggerScan()
+        {
+            Console.WriteLine("Deu certo!!");
+        }
+
+        public void StartScanning()
+        {
+            Scanner device = null;
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                device = listBox1.SelectedItem as Scanner;
+            }));
+
+            if (device == null)
+            {
+                MessageBox.Show("Selecione um scanner",
+                                "Aviso",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }else if(String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Digite um nome",
+                                "Aviso",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ImageFile image = new ImageFile();
+            string imageExtension = "";
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        image = device.ScanImage(WIA.FormatID.wiaFormatPNG);
+                        imageExtension = ".png";
+                        break;
+                    case 1:
+                        image = device.ScanImage(WIA.FormatID.wiaFormatJPEG);
+                        imageExtension = ".jpeg";
+                        break;
+                    case 2:
+                        image = device.ScanImage(WIA.FormatID.wiaFormatBMP);
+                        imageExtension = ".bmp";
+                        break;
+                    case 3:
+                        image = device.ScanImage(WIA.FormatID.wiaFormatGIF);
+                        imageExtension = ".gif";
+                        break;
+                    case 4:
+                        image = device.ScanImage(WIA.FormatID.wiaFormatTIFF);
+                        imageExtension = ".tiff";
+                        break;
+                }
+            }));
+            
+            
+            // Save the image
+            var path = Path.Combine(textBox1.Text, textBox2.Text + imageExtension);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            image.SaveFile(path);
+
+            pictureBox1.Image = new Bitmap(path);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+            }
+        }
+
+    }
+}
---
+++ ScannerDemo/Scanner.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using WIA;
+using System.Windows.Forms;
+
+namespace ScannerDemo
+{
+    class Scanner
+    {
+        const string WIA_SCAN_COLOR_MODE = "6146";
+        const string WIA_HORIZONTAL_SCAN_RESOLUTION_DPI = "6147";
+        const string WIA_VERTICAL_SCAN_RESOLUTION_DPI = "6148";
+        const string WIA_HORIZONTAL_SCAN_START_PIXEL = "6149";
+        const string WIA_VERTICAL_SCAN_START_PIXEL = "6150";
+        const string WIA_HORIZONTAL_SCAN_SIZE_PIXELS = "6151";
+        const string WIA_VERTICAL_SCAN_SIZE_PIXELS = "6152";
+        const string WIA_SCAN_BRIGHTNESS_PERCENTS = "6154";
+        const string WIA_SCAN_CONTRAST_PERCENTS = "6155";
+
+        private readonly DeviceInfo _deviceInfo;
+        private int resolution = 300;
+        private int width_pixel = 1250;
+        private int height_pixel = 1700;
+        private int color_mode = 1;
+
+        public Scanner(DeviceInfo deviceInfo)
+        {
+            this._deviceInfo = deviceInfo;
+        }
+
+        public ImageFile ScanImage(string imageFormat)
+        {
+            var device = this._deviceInfo.Connect();
+
+            CommonDialogClass dlg = new CommonDialogClass(); 
+
+            var item = device.Items[1];
+
+            try
+            {
+                AdjustScannerSettings(item, resolution, 0, 0, width_pixel, height_pixel, 0, 0, color_mode);
+
+                object scanResult = dlg.ShowTransfer(item, imageFormat, true);
+
+                if(scanResult != null)
+                {
+                    var imageFile = (ImageFile)scanResult;
+
+                    // Return the imageFile
+                    return imageFile;
+                }
+            }
+            catch (COMException e)
+            {
+                // Display the exception in the console.
+                Console.WriteLine(e.ToString());
+
+                uint errorCode = (uint)e.ErrorCode;
+
+                // Catch 2 of the most common exceptions
+                if (errorCode ==  0x80210006)
+                {
+                    MessageBox.Show("O scanner não está pronto para uso");
+                }
+                else if(errorCode == 0x80210064)
+                {
+                    MessageBox.Show("O scaneamento foi cancelado.");
+                }
+                else
+                {
+                    MessageBox.Show("Erro:","Error",MessageBoxButtons.OK);
+                }
+            }
+
+            return new ImageFile();
+        }
+
+        private void AdjustScannerSettings(IItem scannnerItem, int scanResolutionDPI, int scanStartLeftPixel, int scanStartTopPixel, int scanWidthPixels, int scanHeightPixels, int brightnessPercents, int contrastPercents, int colorMode)
+        {
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_RESOLUTION_DPI, scanResolutionDPI);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_RESOLUTION_DPI, scanResolutionDPI);
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_START_PIXEL, scanStartLeftPixel);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_START_PIXEL, scanStartTopPixel);
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_SIZE_PIXELS, scanWidthPixels);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_SIZE_PIXELS, scanHeightPixels);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_BRIGHTNESS_PERCENTS, brightnessPercents);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_CONTRAST_PERCENTS, contrastPercents);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_COLOR_MODE, colorMode);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <param name="propName"></param>
+        /// <param name="propValue"></param>
+        /// 
+        private void SetWIAProperty(IProperties properties, object propName, object propValue)
+        {
+            Property prop = properties.get_Item(ref propName);
+
+            try
+            {
+                prop.set_Value(ref propValue);
+            }
+            catch
+            {
+                if (propName.ToString() == WIA_HORIZONTAL_SCAN_RESOLUTION_DPI || propName.ToString() == WIA_VERTICAL_SCAN_RESOLUTION_DPI)
+                {
+                    foreach (object test in prop.SubTypeValues)
+                    {
+                        prop.set_Value(test);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return (string) this._deviceInfo.Properties["Name"].get_Value();
+        }
+         
+    }
+}
